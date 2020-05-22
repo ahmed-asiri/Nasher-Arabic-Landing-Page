4cc1400db5399765b4e58a667502c072f5acfd27
Ahmed Asiri <ia7mad13@gmail.com>
2020-05-22 13:53:27 +0300

    Testing the enhancment solution

--- js/app.js
+++ js/app.js
@@ -1,14 +1,4 @@
-function scrollDownToElement() {
-    let ul = document.querySelector('#nav-bar ul');
-    ul.addEventListener('click', function (eve) {
-        let navElement;
-        if (eve.target.nodeName === 'A') {
-            eve.preventDefault();
-            navElement = document.querySelector('#' + eve.target.id).parentElement;
-            document.querySelector('.' + navElement.id).scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-        }
-    });
-}
+
 
 
 function highlightOnScroll() {
@@ -28,30 +18,71 @@ function highlightOnScroll() {
         }
     });
 }
+// FIRST VERSION, NOT WORKING ON SAFARI AND IE
 
-function scrollDownToElementTest() {
-    let ul = document.querySelector('#nav-bar ul');
-    ul.addEventListener('click', function (eve) {
-        let navElement;
-        if (eve.target.nodeName === 'A') {
-            eve.preventDefault();
-            navElement = document.querySelector('#' + eve.target.id).parentElement;
-            document.querySelector('.' + navElement.id);
-        }
-    });
-}
+// function scrollDownToElement() {
+//     let ul = document.querySelector('#nav-bar ul');
+//     ul.addEventListener('click', function (eve) {
+//         let navElement;
+//         if (eve.target.nodeName === 'A') {
+//             eve.preventDefault();
+//             navElement = document.querySelector('#' + eve.target.id).parentElement;
+//             document.querySelector('.' + navElement.id).scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+//         }
+//     });
+// }
 
-function scrollDownToElementWithoutOptions() {
-    let ul = document.querySelector('#nav-bar ul');
-    ul.addEventListener('click', function (eve) {
-        let navElement;
-        if (eve.target.nodeName === 'A') {
-            eve.preventDefault();
-            navElement = document.querySelector('#' + eve.target.id).parentElement;
-            document.querySelector('.' + navElement.id).scrollIntoView();
-        }
-    });
-}
+
+
+// function scrollDownToElementTest() {
+//     let ul = document.querySelector('#nav-bar ul');
+//     ul.addEventListener('click', function (eve) {
+//         let navElement;
+//         if (eve.target.nodeName === 'A') {
+//             eve.preventDefault();
+//             navElement = document.querySelector('#' + eve.target.id).parentElement;
+//             document.querySelector('.' + navElement.id);
+//         }
+//     });
+// }
+
+// function scrollDownToElementWithoutOptions() {
+//     let ul = document.querySelector('#nav-bar ul');
+//     ul.addEventListener('click', function (eve) {
+//         let navElement;
+//         if (eve.target.nodeName === 'A') {
+//             eve.preventDefault();
+//             navElement = document.querySelector('#' + eve.target.id).parentElement;
+//             document.querySelector('.' + navElement.id).scrollIntoView();
+//         }
+//     });
+// }
+
+// function scrollDownToElementWithoutOptionsAndFixingSafariProblem() {
+//     let anchElements = document.querySelectorAll('#nav-bar ul a');
+//     anchElements[0].parentElement.parentElement.addEventListener('click', function (eve) {
+//         let navElement;
+//         if (eve.target.nodeName === 'A') {
+//             eve.preventDefault();
+//             for(let anchElement of anchElements) {
+//                 if(anchElement.id === eve.target.id){
+//                     navElement = document.querySelector('#' + eve.target.id).parentElement;
+//                     document.querySelector('.' + navElement.id).scrollIntoView();
+//                     setTimeout(function() {
+//                         for(let a of anchElements){
+//                             if(a.id != eve.target.id){
+//                                 a.parentElement.classList.remove("highilgted");
+//                                 console.log('hey');
+//                             }
+//                         } 
+//                     }, 100);
+//                 }
+//             }
+//         }
+//     });
+// }
+
+// SOLUTIONS HERE FINALLY
 
 function scrollDownToElementWithoutOptionsAndFixingSafariProblem() {
     let anchElements = document.querySelectorAll('#nav-bar ul a');
@@ -77,7 +108,30 @@ function scrollDownToElementWithoutOptionsAndFixingSafariProblem() {
     });
 }
 
+//enhacment version
+
+function scrollDownToElementWithoutOptionsAndFixingSafariProblem2() {
+    let ulElement = document.querySelector('#nav-bar ul');
+    ulElement.addEventListener('click', function (eve) {
+        let liElement;
+        let navigatedSection;
+        if (eve.target.nodeName === 'A') {
+            eve.preventDefault();
+            liElement = document.querySelector('#' + eve.target.id).parentElement;
+            navigatedSection = document.querySelector('.' + liElement.id);
+            navigatedSection.scrollIntoView();
+            setTimeout(function() {
+                for(let li of ulElement.childNodes){
+                    if(li.id != eve.target.id){
+                        a.parentElement.classList.remove("highilgted");
+                        console.log('hey');
+                    }
+                } 
+            }, 100);
+        }
+    });
+}
 
 highlightOnScroll();
 
-scrollDownToElementWithoutOptionsAndFixingSafariProblem();
+scrollDownToElementWithoutOptionsAndFixingSafariProblem2();
