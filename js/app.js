968b62f26b34b13475bafc625f3574e70557a197
Ahmed Asiri <ia7mad13@gmail.com>
2020-05-22 15:09:47 +0300

    call-us section added plus navigation with js solved

--- js/app.js
+++ js/app.js
@@ -1,130 +1,38 @@
 
-
-
 function highlightOnScroll() {
-    let sections = document.querySelectorAll('section');
-    document.addEventListener('scroll', function () {
-        for (let section1 of sections) {
-            let sectionCordinate = section1.getBoundingClientRect();
-            let viewPortHieght = window.innerHeight;
-            navElement = document.querySelector('#' + section1.className);
+    let sections = document.querySelectorAll('section');    // Bring all sections so the event listener can loop through it
+    document.addEventListener('scroll', function () {   // event on the document, to keep track when the document have been scrolled.
+        for (let section1 of sections) {    // detect each section cordinate to the viewport to highlighted
+            let sectionCordinate = section1.getBoundingClientRect();    // get the cordinate of element respective to the viewport
+            let viewPortHieght = window.innerHeight;    // get the height of the viewport
+            navElement = document.querySelector('#' + section1.className);  // i give the section a class name that equal to the id of the corrisponding <li> element in the nav bar
             if ((sectionCordinate.top <= viewPortHieght / 2 && sectionCordinate.top >= 0)
                 || sectionCordinate.bottom >= viewPortHieght / 2 && sectionCordinate.bottom <= viewPortHieght) {
-                    navElement.classList.add('highilgted');
+                    navElement.classList.add('highlighted');    // make it highlighted if the element on the viewport
             } else if ((sectionCordinate.bottom <= viewPortHieght / 2 && sectionCordinate.bottom >= 0) ||
                 (sectionCordinate.top <= viewPortHieght && sectionCordinate.top >= viewPortHieght / 2)) {
-                    navElement.classList.remove('highilgted');
-            }
-        }
-    });
-}
-// FIRST VERSION, NOT WORKING ON SAFARI AND IE
-
-// function scrollDownToElement() {
-//     let ul = document.querySelector('#nav-bar ul');
-//     ul.addEventListener('click', function (eve) {
-//         let navElement;
-//         if (eve.target.nodeName === 'A') {
-//             eve.preventDefault();
-//             navElement = document.querySelector('#' + eve.target.id).parentElement;
-//             document.querySelector('.' + navElement.id).scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-//         }
-//     });
-// }
-
-
-
-// function scrollDownToElementTest() {
-//     let ul = document.querySelector('#nav-bar ul');
-//     ul.addEventListener('click', function (eve) {
-//         let navElement;
-//         if (eve.target.nodeName === 'A') {
-//             eve.preventDefault();
-//             navElement = document.querySelector('#' + eve.target.id).parentElement;
-//             document.querySelector('.' + navElement.id);
-//         }
-//     });
-// }
-
-// function scrollDownToElementWithoutOptions() {
-//     let ul = document.querySelector('#nav-bar ul');
-//     ul.addEventListener('click', function (eve) {
-//         let navElement;
-//         if (eve.target.nodeName === 'A') {
-//             eve.preventDefault();
-//             navElement = document.querySelector('#' + eve.target.id).parentElement;
-//             document.querySelector('.' + navElement.id).scrollIntoView();
-//         }
-//     });
-// }
-
-// function scrollDownToElementWithoutOptionsAndFixingSafariProblem() {
-//     let anchElements = document.querySelectorAll('#nav-bar ul a');
-//     anchElements[0].parentElement.parentElement.addEventListener('click', function (eve) {
-//         let navElement;
-//         if (eve.target.nodeName === 'A') {
-//             eve.preventDefault();
-//             for(let anchElement of anchElements) {
-//                 if(anchElement.id === eve.target.id){
-//                     navElement = document.querySelector('#' + eve.target.id).parentElement;
-//                     document.querySelector('.' + navElement.id).scrollIntoView();
-//                     setTimeout(function() {
-//                         for(let a of anchElements){
-//                             if(a.id != eve.target.id){
-//                                 a.parentElement.classList.remove("highilgted");
-//                                 console.log('hey');
-//                             }
-//                         } 
-//                     }, 100);
-//                 }
-//             }
-//         }
-//     });
-// }
-
-// SOLUTIONS HERE FINALLY
-
-function scrollDownToElementWithoutOptionsAndFixingSafariProblem() {
-    let anchElements = document.querySelectorAll('#nav-bar ul a');
-    anchElements[0].parentElement.parentElement.addEventListener('click', function (eve) {
-        let navElement;
-        if (eve.target.nodeName === 'A') {
-            eve.preventDefault();
-            for(let anchElement of anchElements) {
-                if(anchElement.id === eve.target.id){
-                    navElement = document.querySelector('#' + eve.target.id).parentElement;
-                    document.querySelector('.' + navElement.id).scrollIntoView();
-                    setTimeout(function() {
-                        for(let a of anchElements){
-                            if(a.id != eve.target.id){
-                                a.parentElement.classList.remove("highilgted");
-                                console.log('hey');
-                            }
-                        } 
-                    }, 100);
-                }
+                    navElement.classList.remove('highlighted');     // remove highlighted class if the element got out of the viewport
             }
         }
     });
 }
 
-//enhacment version
 
-function scrollDownToElementWithoutOptionsAndFixingSafariProblem2() {
+function scrollToElement() {
     let ulElement = document.querySelector('#nav-bar ul');
     ulElement.addEventListener('click', function (eve) {
-        let liElement;
-        let navigatedSection;
+        let liElement; // this is the <li> element that hold the anchor element user pressed
+        let navigatedSection; // this the targeted section to scroll into it
         if (eve.target.nodeName === 'A') {
-            eve.preventDefault();
-            liElement = document.querySelector('#' + eve.target.id).parentElement;
-            navigatedSection = document.querySelector('.' + liElement.id);
-            navigatedSection.scrollIntoView();
-            setTimeout(function() {
-                let liElements = ulElement.children;
+            eve.preventDefault(); // to prevent anchor from refreshing the page as their default behaviour
+            liElement = document.querySelector('#' + eve.target.id).parentElement; // the <li> is the parent element of the pressed anchor element (<a>)
+            navigatedSection = document.querySelector('.' + liElement.id); // i give the section class equal to the id of the <li> element, to easily determine where is the direction 
+            navigatedSection.scrollIntoView(); // to scroll to the element, we can give him object, to say navigate smooth, but Safari and IE doesn't support, so i decide to use the CSS property (scroll-behavior: smooth;) for smooth on any other browser
+            setTimeout(function() { // i use the timeout to solve the problem of safari and IE, (any other browser don't actually need this part of code, they work well with-out it)
+                let liElements = ulElement.children; 
                 for(let li of liElements){
                     if(li.id != liElement.id){
-                        li.classList.remove("highilgted");
+                        li.classList.remove("highlighted");
                     }
                 } 
             }, 0);
@@ -133,23 +41,23 @@ function scrollDownToElementWithoutOptionsAndFixingSafariProblem2() {
 }
 
 
-function scrollDownToElementWithoutOptionsAndFixingSafariProblem3() {
-    let ulElement = document.querySelector('#nav-bar ul');
-    ulElement.addEventListener('click', function (eve) {
-        let liElement;
-        let navigatedSection;
-        if (eve.target.nodeName === 'A') {
-            eve.preventDefault();
-            liElement = document.querySelector('#' + eve.target.id).parentElement;
-            navigatedSection = document.querySelector('.' + liElement.id);
-            setTimeout(() => {
-                navigatedSection.scrollIntoView();
-            }, 100);
-        }
-    });
-}
+
+// THIS VERSION OF CODE WORK FINE ON ALL BROWSERS EXCEPT (Safari and IE)
+
+// function scrollToElementNotSupportSafariAndIE() {
+//     let ulElement = document.querySelector('#nav-bar ul');
+//     ulElement.addEventListener('click', function (eve) {
+//         let liElement;
+//         let navigatedSection;
+//         if (eve.target.nodeName === 'A') {
+//             eve.preventDefault();
+//             liElement = document.querySelector('#' + eve.target.id).parentElement;
+//             navigatedSection = document.querySelector('.' + liElement.id);
+//                 navigatedSection.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+//         }
+//     });
+// }
 
 
 highlightOnScroll();
-
-scrollDownToElementWithoutOptionsAndFixingSafariProblem2();
+scrollToElement();
