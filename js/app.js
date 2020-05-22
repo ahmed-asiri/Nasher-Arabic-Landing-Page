f7ab300f5437d5b84e31c8227597cb6d5974a363
Ahmed Asiri <ia7mad13@gmail.com>
2020-05-22 12:03:21 +0300

    TEST FOR SAFARI AND

--- js/app.js
+++ js/app.js
@@ -77,6 +77,6 @@ function scrollDownToElementWithoutOptionsAndFixingSafariProblem() {
 }
 
 
+highlightOnScroll();
 
 scrollDownToElementWithoutOptionsAndFixingSafariProblem();
-highlightOnScroll();
