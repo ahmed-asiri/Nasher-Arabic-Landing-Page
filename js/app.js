59e84d002723fd802325ad56873844b5187a8c3d
Ahmed Asiri <ia7mad13@gmail.com>
2020-05-22 16:06:59 +0300

    Handiling blinking nav problem

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ function scrollToElement() {
                         li.classList.remove("highlighted");
                     }
                 } 
-            }, 1000);
+            }, 400);
         }
     });
 }
